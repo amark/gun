6df0f1bccea332e3bf0b2e16a4f7e26f7d5463e6
theory <mark@accelsor.com>
2014-03-31 02:00:13 -0600

    boo

--- shots.js
+++ shots.js
@@ -208,7 +208,7 @@ module.exports = require('theory')
 			(function(){
 				if(opt.src && opt.src.send){
 					var m = {count: 1, how:{gun: 9}};
-					opt.src.send(m);
+					opt.src.send(m); 
 					console.log("send that pumping message!", m);
 				}
 			})();
