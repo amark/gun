85884961e55ca0b21471a5d6ca1a64fb8d809e4e
theory <mark@accelsor.com>
2014-04-01 03:05:23 -0600

    appfog heroku

--- init.js
+++ init.js
@@ -5,10 +5,7 @@ var LIVE = process.env.LIVE || (process.env.NODE_ENV === 'production')
 , web = require('coalesce')
 , opt = {};
 opt.port = process.env.PORT || process.env.OPENSHIFT_NODEJS_POR || process.env.VCAP_APP_PORT || 8888;
-process.env.domain = LIVE? 'http://gunjs.herokuapp.com' : 
-	(function(){require('child_process').exec('ifconfig',function(e,r){
-		console.log('on',process.env.domain='http://'+ r.match(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)[0] +':'+opt.port)
-	})})();
+
 opt.hook = {
 	pre: (function(req,res){
 		//console.log("--------- "+req.flow+" : "+req.url.pathname +" ---------------");
