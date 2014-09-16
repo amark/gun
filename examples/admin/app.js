93e34f364bf7d21ecfc923d61fd6744f8a39e7d2
theory <mark@accelsor.com>
2014-09-16 15:29:07 -0700

    touching app.js

--- examples/admin/app.js
+++ examples/admin/app.js
@@ -1,3 +1,4 @@
+console.log("HEY YOU");
 console.log("If modules not found, run `npm install` in example/admin folder!");
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || 8888;
