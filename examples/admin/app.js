a4e605c75eab79087c5d939fed42488475098270
theory <mark@accelsor.com>
2014-09-16 23:40:18 -0700

    finally heroku!

--- examples/admin/app.js
+++ examples/admin/app.js
@@ -1,6 +1,4 @@
-console.log("HEY YOU please work?");
 console.log("If modules not found, run `npm install` in example/admin folder!");
-
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || 8888;
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -18,8 +16,5 @@ app.listen(port);
 console.log('Express started on port ' + port + ' with /gun');
 
 gun.load('blob/data').blank(function(){
-	gun.set({
-		hello: "world",
-		from: "Mark Nadal"
-	}).key('blob/data');
+	gun.set({ hello: "world", from: "Mark Nadal" }).key('blob/data');
 });
