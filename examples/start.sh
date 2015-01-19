19b5b89b7c3f880cbad79fdf6a352d2b09c70e0f
Mark Nadal <mark@accelsor.com>
2015-01-18 22:11:10 -0700

    add readme comments

--- examples/start.sh
+++ examples/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# README
+# This will install nodejs and npm on your system,
+# should work on most places other than Windows.
+# If you are on Windows, http://nodejs.org/download/ has
+# a installer that will automatically do it for you.
+# Copy paste and run each line into your terminal.
+
 #debian/ubuntu
 sudo apt-get update -y
 sudo apt-get install curl git git-core -y
@@ -10,6 +17,7 @@ sudo yum install curl git git-core -y
 #install nodejs
 git clone http://github.com/isaacs/nave.git
 sudo ./nave/nave.sh usemain stable
+# If you just want nodejs and npm but NOT gun, stop here.
 
 npm install gun
 
