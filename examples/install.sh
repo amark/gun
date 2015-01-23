56fd423090bd57b2a31642ac5ba847038be0aec2
Mark Nadal <mark@accelsor.com>
2015-01-22 18:47:03 -0700

    better comments

--- examples/install.sh
+++ examples/install.sh
@@ -3,9 +3,9 @@
 # README
 # This will install nodejs and npm on your system,
 # should work on most places other than Windows.
+# Copy paste and run each line into your terminal.
 # If you are on Windows, http://nodejs.org/download/ has
 # a installer that will automatically do it for you.
-# Copy paste and run each line into your terminal.
 
 #debian/ubuntu
 sudo apt-get update -y
@@ -14,15 +14,14 @@ sudo apt-get install curl git git-core -y
 sudo yum check-update -y
 sudo yum install curl git git-core -y
 
-#install nodejs
+# install nodejs
 git clone http://github.com/isaacs/nave.git
 sudo ./nave/nave.sh usemain stable
-# If you just want nodejs and npm but NOT gun, stop here.
+# If you just want nodejs and npm but not gun, stop here.
 
 npm install gun
 
+# to run the gun examples:
 cd ./node_modules/gun/examples
-
 npm install .
-
-sudo /usr/local/bin/node ./express.js
+sudo /usr/local/bin/node ./express.js 80 # change `80` to `8080` for local testing.
