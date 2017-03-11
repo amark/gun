c64f827f9d3a838df735d0cff75929e12d97d8e7
Mark Nadal <mark@accelsor.com>
2017-03-11 07:15:29 -0800

    load testing!!! 1.7K inserts/sec! Only will get more better!

--- test/common.js
+++ test/common.js
@@ -3543,9 +3543,9 @@ describe('Gun', function(){
 			var gunB = Gun( { file : "B.json" });
 			var gunC = Gun( { file : "C.json" });
 
-			gunA.get( "some path A" ).map( (v,f)=>{ console.log( "event on A: ", f, v ) } );
-			gunB.get( "some path B" ).map( (v,f)=>{ console.log( "event on B: ", f, v ) } );
-			gunC.get( "some path C" ).map( (v,f)=>{ console.log( "event on C: ", f, v ) } );
+			gunA.get( "some path A" ).map(function(v,f){ console.log( "event on A: ", f, v ) } );
+			gunB.get( "some path B" ).map(function(v,f){ console.log( "event on B: ", f, v ) } );
+			gunC.get( "some path C" ).map(function(v,f){ console.log( "event on C: ", f, v ) } );
 
 			gunA.get( "some path A" ).put( { simple:"message" } );
 			gunB.get( "some path B" ).put( { simple:"message" } );
