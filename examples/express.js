2d455d30b5ac69fbdf696d3d7344189965dc6e37
Mark Nadal <mark@accelsor.com>
2017-03-13 19:46:12 -0700

    fix express

--- examples/express.js
+++ examples/express.js
@@ -2,11 +2,14 @@ console.log("If modules not found, run `npm install` in /example folder!"); // g
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8080;
 
 var express = require('express');
+var http = require('http');
 var app = express();
+var server = http.createServer(app);
 
 var Gun = require('../');
 var gun = Gun({
 	file: 'data.json',
+	web: server,
 	s3: {
 		key: '', // AWS Access Key
 		secret: '', // AWS Secret Token
@@ -14,7 +17,8 @@ var gun = Gun({
 	}
 });
 
-gun.wsp(app);
-app.use(express.static(__dirname)).listen(port);
+app.use(Gun.serve);
+app.use(express.static(__dirname));
+server.listen(port);
 
 console.log('Server started on port ' + port + ' with /gun');
--- src/set.js
+++ src/set.js
@@ -4,8 +4,11 @@ Gun.chain.set = function(item, cb, opt){
 	var gun = this, soul;
 	cb = cb || function(){};
 	if(soul = Gun.node.soul(item)){ return gun.set(gun.back(-1).get(soul), cb, opt) }
-	if(Gun.obj.is(item) && !Gun.is(item)){ return gun.set(gun._.root.put(item), cb, opt) }
-	return item.get('_').get(function(at, ev){
+	if(!Gun.is(item)){
+		if(Gun.obj.is(item)){ return gun.set(gun._.root.put(item), cb, opt) }
+		return gun.get(Gun.text.random()).put(item);
+	}
+	item.get('_').get(function(at, ev){
 		if(!at.gun || !at.gun._.back);
 		ev.off();
 		at = (at.gun._.back._);
@@ -13,5 +16,6 @@ Gun.chain.set = function(item, cb, opt){
 		if(!soul){ return cb.call(gun, {err: Gun.log('Only a node can be linked! Not "' + node + '"!')}) }
 		gun.put(Gun.obj.put(put, soul, Gun.val.rel.ify(soul)), cb, opt);
 	},{wait:0});
+	return item;
 }
 	
