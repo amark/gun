029e11e907bcebf7d3c7c1dd736611dd2d384c7c
Mark Nadal <mark@accelsor.com>
2017-03-13 17:37:39 -0700

    bump set

--- gun.js
+++ gun.js
@@ -2217,8 +2217,11 @@
 			var gun = this, soul;
 			cb = cb || function(){};
 			if(soul = Gun.node.soul(item)){ return gun.set(gun.back(-1).get(soul), cb, opt) }
-			if(Gun.obj.is(item) && !Gun.is(item)){ return gun.set(gun._.root.put(item), cb, opt) }
-			return item.get('_').get(function(at, ev){
+			if(!Gun.is(item)){
+				if(Gun.obj.is(item)){ return gun.set(gun._.root.put(item), cb, opt) }
+				return gun.get(Gun.text.random()).put(item);
+			}
+			item.get('_').get(function(at, ev){
 				if(!at.gun || !at.gun._.back);
 				ev.off();
 				at = (at.gun._.back._);
@@ -2226,6 +2229,7 @@
 				if(!soul){ return cb.call(gun, {err: Gun.log('Only a node can be linked! Not "' + node + '"!')}) }
 				gun.put(Gun.obj.put(put, soul, Gun.val.rel.ify(soul)), cb, opt);
 			},{wait:0});
+			return item;
 		}
 	})(require, './set');
 
