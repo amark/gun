01be81119a44511f503002a5d5f1cd2e76b3f930
d3x0r <d3x0r@users.noreply.github.com>
2017-03-22 17:40:55 -0700

    opts are in a different location

--- gun.js
+++ gun.js
@@ -2376,7 +2376,7 @@
 		function open(peer, as){
 			if(!peer || !peer.url){ return }
 			var url = peer.url.replace('http', 'ws');
-			var wire = peer.wire = new WebSocket(url, as.wsp.wsc.protocols, as.wsp.wsc );
+			var wire = peer.wire = new WebSocket(url, as.opt.wsc.protocols, as.opt.wsc );
 			wire.onclose = function(){
 				reconnect(peer, as);
 			};
