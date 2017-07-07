c03a03c2c1dc77d155ad88197c7f77981b2b6e77
Mark Nadal <mark@accelsor.com>
2017-07-07 00:29:20 -0700

    some quick fixes

--- nts.js
+++ nts.js
@@ -1,5 +1,7 @@
 ;(function(){
-
+	// NOTE: While the algorithm is P2P,
+	// the current implementation is one sided,
+	// only browsers self-modify, servers do not.
 	var env;
 	if(typeof global !== "undefined"){ env = global }
 	if(typeof window !== "undefined"){ var Gun = (env = window).Gun }
@@ -7,11 +9,10 @@
 	if(typeof require !== "undefined"){ var Gun = require('./gun') }
 	}
 
-	Gun.on('opt', function(at){
-		this.to.next(at);
-		if(at.once){ return }
-		var root = at.gun;
-		root.on('in', function(at){
+	Gun.on('opt', function(ctx){
+		this.to.next(ctx);
+		if(ctx.once){ return }
+		ctx.on('in', function(at){
 			if(!at.NTS){
 				return this.to.next(at);
 			}
@@ -24,13 +25,12 @@
 			}
 			this.to.next({'@': at['#'], NTS: Gun.time.is()});
 		});
-
 		var ask = {}, noop = function(){};
 		if(!env.window){ return }
 
 		Gun.state.drift = Gun.state.drift || 0;
-		function ping(){
-			var NTS = {}, ack = Gun.text.random(), msg = {'#': ack, NTS: true, gun: root};
+		setTimeout(function ping(){
+			var NTS = {}, ack = Gun.text.random(), msg = {'#': ack, NTS: true, gun: ctx.gun};
 			NTS.start = Gun.state();
 			ask[ack] = function(at){
 				NTS.end = Gun.state();
@@ -41,8 +41,8 @@
 				Gun.state.drift -= (NTS.end - NTS.calc)/2;
 				setTimeout(ping, 1000);
 			}
-			Gun.on('out', msg);
-		}; ping();
+			ctx.on('out', msg);
+		}, 1);
 	});
 	// test by opening up examples/game/nts.html on devices that aren't NTP synced.
 }());
