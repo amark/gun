9ea1808f5e97fe66e39ebdc9b3c39ad927ae4731
Brock Atkinson <brockatkinson@hotmail.com>
2018-02-01 18:12:31 +0800

    update examples\hapi.js to suit v17

--- examples/hapi.js
+++ examples/hapi.js
@@ -1,30 +1,55 @@
 console.log("If module not found, install hapi globally `npm i hapi inert -g`!");
+
 const Hapi  = require('hapi')
 const Inert = require('inert')
 const Gun   = require('..')
 
-const server = new Hapi.Server
-server.connection({ port: 8080 })
-server.connections.forEach(c => Gun({ web: c.listener, file: 'data.json' }))
-
-server.register(Inert, () => {});
-
-server.route({
-  method: 'GET',
-  path: '/gun.js',
-  handler: (request, reply) => reply.file('../gun.js', { confine: false })
-})
-
-server.route({
-  method: 'GET',
-  path: '/{param*}',
-  handler: {
-    directory: {
-      path: __dirname,
-      redirectToSlash: true,
-      index: true
+const server = new Hapi.Server({
+  port: 8080,
+  host: 'localhost',
+  routes: {
+    files: {
+      relativeTo: require('path').join(__dirname, '..')
     }
   }
 })
 
-server.start()
+async function runtime(db) {
+
+  await server.register(Inert);
+
+  // server.connections.forEach(c => Gun({ web: c.listener, file: 'data.json' }))
+
+  server.route({
+    method: 'GET',
+    path: '/gun.js',
+    handler: {
+      file:  'gun.min.js'
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/gun/nts.js',
+    handler: {
+      file:  'nts.js'
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: __dirname,
+        redirectToSlash: true,
+        index: true
+      }
+    }
+  })
+
+  await server.start();
+  console.log('Server running at:', server.info.uri, 'dir:', __dirname)
+}
+
+runtime()
