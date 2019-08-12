f3364b0090397cf12731452a4a29f7e72055d6c3
Rob <rm.rf.etc@gmail.com>
2019-08-11 20:16:39 -0700

    Use require.context() to fix webpack warning

--- gun.js
+++ gun.js
@@ -1,22 +1,23 @@
 ;(function(){
 
-  /* UNBUILD */
-  var root;
-  if(typeof window !== "undefined"){ root = window }
-  if(typeof global !== "undefined"){ root = global }
-  root = root || {};
-  var console = root.console || {log: function(){}};
-  function USE(arg, req){
-    return req? require(arg) : arg.slice? USE[R(arg)] : function(mod, path){
-      arg(mod = {exports: {}});
-      USE[R(path)] = mod.exports;
-    }
-    function R(p){
-      return p.split('/').slice(-1).toString().replace('.js','');
-    }
-  }
-  if(typeof module !== "undefined"){ var common = module }
-  /* UNBUILD */
+	/* UNBUILD */
+	var root;
+	if(typeof window !== "undefined"){ root = window }
+	if(typeof global !== "undefined"){ root = global }
+	root = root || {};
+	var console = root.console || {log: function(){}};
+	function USE(arg, req){
+		var _require = require.context('./src', true, /./);
+		return req? _require(arg) : arg.slice? USE[R(arg)] : function(mod, path){
+			arg(mod = {exports: {}});
+			USE[R(path)] = mod.exports;
+		}
+		function R(p){
+			return p.split('/').slice(-1).toString().replace('.js','');
+		}
+	}
+	if(typeof module !== "undefined"){ var common = module }
+	/* UNBUILD */
 
 	;USE(function(module){
 		// Generic javascript utilities.
