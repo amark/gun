68f4308664ccbf95028fad94b14516ff0ef622f0
Mark Nadal <mark@gun.eco>
2019-09-17 01:53:56 -0700

    loopit

--- gun.js
+++ gun.js
@@ -1295,19 +1295,18 @@
 			if(tmp = cat.soul || cat.link || cat.dub){ return cb(tmp, as, cat) }
 			if(cat.jam){ return cat.jam.push([cb, as]) }
 			cat.jam = [[cb,as]];
-			gun.get(function(msg, eve){
+			gun.get(function go(msg, eve){
 				if(u === msg.put && (tmp = Object.keys(cat.root.opt.peers).length) && ++acks < tmp){
 					return;
 				}
 				eve.rid(msg);
-				var at = ((at = msg.$) && at._) || {};
-				tmp = cat.jam; Gun.obj.del(cat, 'jam');
-				Gun.obj.map(tmp, function(as, cb){
-					cb = as[0]; as = as[1];
-					if(!cb){ return }
-					var id = at.link || at.soul || rel.is(msg.put) || node_soul(msg.put) || at.dub;
-					cb(id, as, msg, eve);
-				});
+				var at = ((at = msg.$) && at._) || {}, i = 0, as;
+				tmp = cat.jam; delete cat.jam; // tmp = cat.jam.splice(0, 100);
+				//if(tmp.length){ process.nextTick(function(){ go(msg, eve) }) }
+				while(as = tmp[i++]){ //Gun.obj.map(tmp, function(as, cb){
+					var cb = as[0], id; as = as[1];
+					cb && cb(id = at.link || at.soul || rel.is(msg.put) || node_soul(msg.put) || at.dub, as, msg, eve);
+				} //);
 			}, {out: {get: {'.':true}}});
 			return gun;
 		}
@@ -1701,7 +1700,7 @@
 				return;
 			}
 			if(link && u === link.put && (tmp = rel.is(data))){ data = Gun.node.ify({}, tmp) }
-			eve.rid(msg);
+			eve.rid? eve.rid(msg) : eve.off();
 			opt.ok.call(gun || opt.$, data, msg.get);
 		}
 
