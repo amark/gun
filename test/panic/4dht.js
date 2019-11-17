e048b897da91a215add55e2b0826154414e5b196
Mark Nadal <mark@gun.eco>
2019-11-17 03:35:12 -0800

    stuff

--- test/panic/4dht.js
+++ test/panic/4dht.js
@@ -2,20 +2,17 @@
 This is the first in a series of basic networking correctness tests.
 Each test itself might be dumb and simple, but built up together,
 they prove desired end goals for behavior at scale.
-1. (this file) Is a browser write is confirmed as save by multiple peers even if by daisy chain.
-2. 
 */
 
 var config = {
 	IP: require('ip').address(),
 	port: 8765,
-	servers: 1,
+	servers: 2,
 	browsers: 2,
 	route: {
 		'/': __dirname + '/index.html',
 		'/gun.js': __dirname + '/../../gun.js',
-		'/jquery.js': __dirname + '/../../examples/jquery.js',
-		'/livestream.html': __dirname + '/livestream.html',
+		'/jquery.js': __dirname + '/../../examples/jquery.js'
 	}
 }
 
@@ -39,11 +36,12 @@ manager.start({
 
 var servers = clients.filter('Node.js');
 var bob = servers.pluck(1);
+var carl = servers.excluding(bob).pluck(1);
 var browsers = clients.excluding(servers);
 var alice = browsers.pluck(1);
-var others = browsers.excluding(alice);
+var dave = browsers.excluding(alice).pluck(1);
 
-describe("Broadcast Video", function(){
+describe("Put ACK", function(){
 	//this.timeout(5 * 60 * 1000);
 	this.timeout(10 * 60 * 1000);
 
@@ -86,34 +84,8 @@ describe("Broadcast Video", function(){
 		return browsers.atLeast(config.browsers);
 	});
 
-	it("Browsers load QVDev's streaming!", function(){
-		var tests = [], i = 0;
-		browsers.each(function(client, id){
-			tests.push(client.run(function(test){
-				test.async();
-				console.log("load?");
-				function load(src, cb){
-					var script = document.createElement('script');
-					script.onload = cb; script.src = src;
-					document.head.appendChild(script);
-				}
-				load('https://cdn.jsdelivr.net/gh/QVDev/GunStreamer/js/GunRecorder.js', function(){
-					load('https://cdn.jsdelivr.net/gh/QVDev/GunStreamer/js/GunStreamer.js', function(){
-						load('https://cdn.jsdelivr.net/gh/QVDev/GunStreamer/js/GunViewer.js', function(){
-							load('https://cdn.jsdelivr.net/gh/QVDev/GunStreamer/js/mediabuffer.js', function(){
-								test.done();
-							});
-						});
-					});
-				});
-				$('body').append('<video id="video" width="100%" autoplay></video>');
-			}, {i: i += 1, config: config})); 
-		});
-		return Promise.all(tests);
-	});
-
 	it("Browsers initialized gun!", function(){
-		var tests = [], i = 0;
+		var tests = [], i = 1;
 		browsers.each(function(client, id){
 			tests.push(client.run(function(test){
 				try{ localStorage.clear() }catch(e){}
@@ -121,62 +93,34 @@ describe("Broadcast Video", function(){
 				var env = test.props;
 				var gun = Gun('http://'+ env.config.IP + ':' + (env.config.port + 1) + '/gun');
 				window.gun = gun;
+				window.ref = gun.get('test');
 			}, {i: i += 1, config: config})); 
 		});
 		return Promise.all(tests);
 	});
 
-	it("Stream", function(){
+	it("Alice", function(){
 		return alice.run(function(test){
 			console.log("I AM ALICE");
-			test.async();
-			var stream = window.stream = new GunStreamer({
-				url: "https://cdn.jsdelivr.net/gh/QVDev/GunStreamer/js/parser_worker.js",
-				gun: gun,
-				streamId: 'livestream',
-				dbRecord: 'streams'
-			});
-			var record = window.record = new GunRecorder({
-				mimeType: 'video/webm; codecs="opus,vp8"',
-				//audioBitsPerSecond: 6000,//Audio bits per second this is the lowest quality
-				//videoBitsPerSecond: 100000,//Video bits per second this is the lowest quality
-				cameraOptions: {video:{width: 1280, height: 720, facingMode: "environment", frameRate: 60}, audio: false},
-				video_id: "video",
-				recordInterval: 1000, // how long each chunk?
-				onRecordStateChange: function(state){ /* change play/pause buttons */ },
-				onDataAvailable: function(data){ stream.onDataAvailable(data) } // pass recorded data to streamer
-			});
-			record.startCamera();
-			$('#video').on('playing', function(eve){
-			  record.record();
-			  test.done();
-			});
-  		console.log("start recording!");
-		}, {acks: config.servers});
+			//test.async();
+			console.log(gun._.opt.peers);
+		});
 	});
 
-	it("View", function(){
-		return others.run(function(test){
-			console.log("I AM A VIEWER!");
-			test.async();
-			var view = new GunViewer({
-				//mimeType: 'video/webm; codecs="opus,vp8"', // NEED THIS ONE FOR AUDIO+VIDEO
-				mimeType: 'video/webm; codecs="vp8"',
-				streamerId: "video",
-				debug: true,//For debug logs  
-			});
-			gun.get('livestream').on(function(data){
-				window.data = data;
-				view.onStreamerData(data);
-			});
-		}, {acks: config.servers});
+	it("Dave", function(){
+		return dave.run(function(test){
+			console.log("I AM DAVE");
+			//test.async();
+			console.log(gun._.opt.peers);
+		});
 	});
 
 	it("All finished!", function(done){
 		console.log("Done! Cleaning things up...");
 		setTimeout(function(){
 			done();
-		},1000);
+		//},1000);
+		},1000 * 60);
 	});
 
 	after("Everything shut down.", function(){
