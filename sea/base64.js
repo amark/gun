ef49a73e4a22dcc21ed7115ba9a319ff904d61ac
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-21 09:12:56 +0200

    fix: bad comparison

--- sea/base64.js
+++ sea/base64.js
@@ -2,7 +2,7 @@
     if(typeof Buffer === "undefined") {
       root.Buffer = require("buffer").Buffer
     }
-    if(typeof btoa !== "undefined"){
+    if(typeof btoa === "undefined"){
       root.btoa = function (data) { return Buffer.from(data, "binary").toString("base64"); };
       root.atob = function (data) { return Buffer.from(data, "base64").toString("binary"); };
     }
