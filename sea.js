b2709e2eb7a0dbe5aeb1a60537cab1f04347c471
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-29 11:42:47 +0200

    fix: only include buffer if no btoa/atob

--- sea.js
+++ sea.js
@@ -48,10 +48,10 @@
   })(USE, './https');
 
   ;USE(function(module){
-    if(typeof Buffer === "undefined") {
-      root.Buffer = require("buffer").Buffer
-    }
     if(typeof btoa === "undefined"){
+      if(typeof Buffer === "undefined") {
+        root.Buffer = require("buffer").Buffer
+      }
       root.btoa = function (data) { return Buffer.from(data, "binary").toString("base64"); };
       root.atob = function (data) { return Buffer.from(data, "base64").toString("binary"); };
     }
--- sea/base64.js
+++ sea/base64.js
@@ -1,8 +1,8 @@
 
-    if(typeof Buffer === "undefined") {
-      root.Buffer = require("buffer").Buffer
-    }
     if(typeof btoa === "undefined"){
+      if(typeof Buffer === "undefined") {
+        root.Buffer = require("buffer").Buffer
+      }
       root.btoa = function (data) { return Buffer.from(data, "binary").toString("base64"); };
       root.atob = function (data) { return Buffer.from(data, "base64").toString("binary"); };
     }
