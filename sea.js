512d2c8c320de00b3c52497044f48e02876670a8
sirpy <hadar@gooddollar.org>
2019-12-08 17:49:48 +0200

    add: wait for once, so data is returned on first get (#6)
    * add: wait for once, so data is returned on first get
    
    * Update then.js

--- sea.js
+++ sea.js
@@ -710,9 +710,10 @@
 
   ;USE(function(module){
     var Gun = USE('./sea').Gun;
-    Gun.chain.then = function(cb){
+    Gun.chain.then = function(cb, opt = {}){
+      opt = {wait: 200, ...opt}
       var gun = this, p = (new Promise(function(res, rej){
-        gun.once(res);
+        gun.once(res, opt);
       }));
       return cb? p.then(cb) : p;
     }
