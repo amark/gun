b3aa9457b097221b70b55f65d07cd0b7b2b465de
genderev <60559370+genderev@users.noreply.github.com>
2020-08-25 22:00:07 +0000

    Update gunwebworker.js

--- gunwebworker.js
+++ gunwebworker.js
@@ -3355,33 +3355,3 @@ Gun.on('create', function(root){
   }catch(e){}
 
 }());
-// lib/erase
-var Gun = (typeof self !== "undefined")? self.Gun : require('../gun');
-
-Gun.on('opt', function(root){
-	this.to.next(root);
-	if(root.once){ return }
-	root.on('put', function(msg){
-		Gun.graph.is(msg.put, null, function(val, key, node, soul){
-			if(null !== val){ return }
-			// TODO: Refactor this to use `.off()`?
-			var tmp = root.graph[soul];
-			if(tmp){
-				delete tmp[key];
-			}
-			tmp = tmp && tmp._ && tmp._['>'];
-			if(tmp){
-				delete tmp[key];
-			}
-			tmp = root.next;
-			if(tmp && (tmp = tmp[soul]) && (tmp = tmp.put)){
-				delete tmp[key];
-				tmp = tmp._ && tmp._['>'];
-				if(tmp){
-					delete tmp[key];
-				}
-			}
-		});
-		this.to.next(msg);
-	});
-});
