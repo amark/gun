946a78a1b727241ec99f4cff89cea0042d8c7b71
Ender Minyard <60559370+genderev@users.noreply.github.com>
2020-08-31 21:23:41 +0000

    Delete worker2.js

--- examples/web worker/worker2.js
+++
@@ -1,5 +0,0 @@
-importScripts("workerdb.js");
-var gun = Gun(["https://mvp-gun.herokuapp.com/gun", "https://e2eec.herokuapp.com/gun"]).get('thoughts');
-self.onmessage = function (message) {
-  gun.set(message.data)
-}
