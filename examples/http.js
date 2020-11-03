ca003747b7d92e8694351dd8a40a612381725a2f
Martti Malmi <sirius@iki.fi>
2020-11-03 17:01:27 +0200

    use PEERS env variable in http.js

--- examples/http.js
+++ examples/http.js
@@ -5,8 +5,13 @@
 	}
 
 	var fs = require('fs');
-	var config = { port: process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765 };
+	var config = {
+		port: process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765,
+		peers: process.env.PEERS && process.env.PEERS.split(',') || []
+	};
 	var Gun = require('../'); // require('gun')
+
+	console.log(config.peers);
 
 	if(process.env.HTTPS_KEY){
 		config.key = fs.readFileSync(process.env.HTTPS_KEY);
@@ -16,7 +21,7 @@
 		config.server = require('http').createServer(Gun.serve(__dirname));
 	}
 
-	var gun = Gun({web: config.server.listen(config.port)});
+	var gun = Gun({web: config.server.listen(config.port), peers: config.peers});
 	console.log('Relay peer started on port ' + config.port + ' with /gun');
 
 	module.exports = gun;
