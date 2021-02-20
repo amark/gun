8428fc10a194758df1020a60437c4a32ce491c89
Yang Hanlin <mattoncis@hotmail.com>
2021-02-20 22:21:27 +0800

    Warn on automatic redirect to HTTPS

--- sea.js
+++ sea.js
@@ -36,6 +36,7 @@
       if(location.protocol.indexOf('s') < 0
       && location.host.indexOf('localhost') < 0
       && location.protocol.indexOf('file:') < 0){
+        console.warn('WebCrypto used by GUN SEA implementation does not work without HTTPS. Will automatically redirect.')
         location.protocol = 'https:'; // WebCrypto does NOT work without HTTPS!
       }
     } }catch(e){}
