0a56ad850fc5e6ebf9bdb11948d605d776a5dd66
Orimay <kender@cyber-wizard.com>
2022-01-14 02:44:31 +0300

    Adding '-Infinity' as an invalid case
    Adding '-Infinity' as an invalid case, not handled before
    Skipping extra checks until needed (like NaN, Infinity)
    Checking for 'null' first as for more common case than invalid 'undefined'

--- src/valid.js
+++ src/valid.js
@@ -1,16 +1,20 @@
 
 require('./shim');
-module.exports = function(v){ // Valid values are a subset of JSON: null, binary, number (!Infinity), text, or a soul relation. Arrays need special algorithms to handle concurrency, so they are not supported directly. Use an extension that supports them if needed but research their problems first.
-	if(v === undefined){ return false }
-	if(v === null){ return true } // "deletes", nulling out keys.
-	if(v === Infinity){ return false } // we want this to be, but JSON does not support it, sad face.
-	if(v !== v){ return false } // can you guess what this checks for? ;)
-	if('string' == typeof v // text!
-	|| 'boolean' == typeof v
-	|| 'number' == typeof v){
-		return true; // simple values are valid.
-	}
-	if(v && ('string' == typeof (v['#']||0)) && Object.empty(v, ['#'])){ return v['#'] } // is link
-	return false; // If not, everything else remaining is an invalid data type. Custom extensions can be built on top of these primitives to support other types.
-}
-	
+
+// Valid values are a subset of JSON: null, binary, number (!Infinity), text,
+// or a soul relation. Arrays need special algorithms to handle concurrency,
+// so they are not supported directly. Use an extension that supports them if
+// needed but research their problems first.
+module.exports = (v) =>
+  // "deletes", nulling out keys.
+  v === null ||
+  (v !== undefined &&
+    ("string" === typeof v ||
+      "boolean" === typeof v ||
+      // we want +/- Infinity to be, but JSON does not support it, sad face.
+      // can you guess what v === v checks for? ;)
+      ("number" === typeof v && v != Infinity && v != -Infinity && v === v) ||
+      (v &&
+        "string" == typeof v["#"] &&
+        Object.keys(v).length === 1 &&
+        v["#"])));
