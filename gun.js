4413cfa8bf320c8a3881b781c58dd848cd683312
dbaranov <dbaranov@bellintegrator.com>
2022-02-04 13:32:16 +0300

    ES5 compatibility

--- gun.js
+++ gun.js
@@ -134,15 +134,16 @@
 		// or a soul relation. Arrays need special algorithms to handle concurrency,
 		// so they are not supported directly. Use an extension that supports them if
 		// needed but research their problems first.
-		module.exports = (v) =>
+		module.exports = function (v) {
 		  // "deletes", nulling out keys.
-		  v === null ||
-		  "string" === typeof v ||
-		  "boolean" === typeof v ||
-		  // we want +/- Infinity to be, but JSON does not support it, sad face.
-		  // can you guess what v === v checks for? ;)
-		  ("number" === typeof v && v != Infinity && v != -Infinity && v === v) ||
-		  (v && "string" == typeof v["#"] && Object.keys(v).length === 1 && v["#"]);
+		  return v === null ||
+			"string" === typeof v ||
+			"boolean" === typeof v ||
+			// we want +/- Infinity to be, but JSON does not support it, sad face.
+			// can you guess what v === v checks for? ;)
+			("number" === typeof v && v != Infinity && v != -Infinity && v === v) ||
+			(v && "string" == typeof v["#"] && Object.keys(v).length === 1 && v["#"]);
+		}
 	})(USE, './valid');
 
 	;USE(function(module){
--- src/valid.js
+++ src/valid.js
@@ -2,12 +2,13 @@
 // or a soul relation. Arrays need special algorithms to handle concurrency,
 // so they are not supported directly. Use an extension that supports them if
 // needed but research their problems first.
-module.exports = (v) =>
+module.exports = function (v) {
   // "deletes", nulling out keys.
-  v === null ||
-  "string" === typeof v ||
-  "boolean" === typeof v ||
-  // we want +/- Infinity to be, but JSON does not support it, sad face.
-  // can you guess what v === v checks for? ;)
-  ("number" === typeof v && v != Infinity && v != -Infinity && v === v) ||
-  (v && "string" == typeof v["#"] && Object.keys(v).length === 1 && v["#"]);
+  return v === null ||
+    "string" === typeof v ||
+    "boolean" === typeof v ||
+    // we want +/- Infinity to be, but JSON does not support it, sad face.
+    // can you guess what v === v checks for? ;)
+    ("number" === typeof v && v != Infinity && v != -Infinity && v === v) ||
+    (v && "string" == typeof v["#"] && Object.keys(v).length === 1 && v["#"]);
+}
