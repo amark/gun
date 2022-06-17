27188828877266473db8d5d3a13af42a69a76d2f
Bradley Matusiak <bmatusiak@gmail.com>
2022-06-17 14:26:38 +0000

    update src/index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 
 var Gun = require('./root');
+require('./shim');
+require('./onto');
+require('./valid');
+require('./state');
+require('./dup');
+require('./ask');
 require('./chain');
 require('./back');
 require('./put');
 require('./get');
+require('./on');
+require('./map');
+require('./set');
+require('./mesh');
+require('./websocket');
+require('./localStorage');
 module.exports = Gun;
 	
