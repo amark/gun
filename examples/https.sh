48b5a0e765c8e9a64c4eb93dd0b0bd196aa4d58a
Mark Nadal <mark@gun.eco>
2023-04-25 19:41:34 -0700

    what about now?

--- examples/https.sh
+++ examples/https.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-echo $EMAIL
-echo $WEBROOT
-echo $DOMAIN
-
 cd ~
 git clone https://github.com/acmesh-official/acme.sh.git
-bash ./acme.sh/acme.sh --install -m $EMAIL
-bash ./acme.sh/acme.sh --issue -d $DOMAIN -w $WEBROOT
-bash ./acme.sh/acme.sh --install-cert -d $DOMAIN --key-file ~/key.pem --fullchain-file ~/cert.pem --reloadcmd "service relay force-reload"~
+cd ~/acme.sh
+./acme.sh --install -m $EMAIL
+
+bash ~/acme.sh/acme.sh --issue -d $DOMAIN -w $WEB
+bash ~/acme.sh/acme.sh --install-cert -d $DOMAIN --key-file ~/key.pem --fullchain-file ~/cert.pem --reloadcmd "service relay force-reload"
