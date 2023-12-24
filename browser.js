265fe864fc690b01ba128e978cc7c136a6a52b05
Bradley Matusiak <bmatusiak@gmail.com>
2023-12-24 11:51:32 -0500

    react-native detection

--- browser.js
+++ browser.js
@@ -1,1 +1,5 @@
-module.exports = require('./gun.js')
+if(!(typeof navigator == "undefined") && navigator.product == "ReactNative"){
+    module.exports = require('./src');
+}else{
+    module.exports = require('./gun.js');
+}
