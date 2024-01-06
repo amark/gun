f04e2fa7199cb0dba255743aa95280aca5590a4c
Bradley Matusiak <bmatusiak@gmail.com>
2024-01-06 08:30:52 -0500

    null check is last

--- sea.js
+++ sea.js
@@ -38,8 +38,8 @@
       && location.host.indexOf('localhost') < 0
       && ! /^127\.\d+\.\d+\.\d+$/.test(location.hostname)
       && location.protocol.indexOf('blob:') < 0
-      && location.origin != 'null'
-      && location.protocol.indexOf('file:') < 0){
+      && location.protocol.indexOf('file:') < 0
+      && location.origin != 'null'){
         console.warn('HTTPS needed for WebCrypto in SEA, redirecting...');
         location.protocol = 'https:'; // WebCrypto does NOT work without HTTPS!
       }
