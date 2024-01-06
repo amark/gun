4ef4376de4a003c25feb26be43078a6ca6e3e377
Bradley Matusiak <bmatusiak@gmail.com>
2024-01-06 07:40:38 -0500

    sea blobs!

--- sea.js
+++ sea.js
@@ -37,6 +37,7 @@
       if(location.protocol.indexOf('s') < 0
       && location.host.indexOf('localhost') < 0
       && ! /^127\.\d+\.\d+\.\d+$/.test(location.hostname)
+      && location.protocol.indexOf('blob:') < 0
       && location.protocol.indexOf('file:') < 0){
         console.warn('HTTPS needed for WebCrypto in SEA, redirecting...');
         location.protocol = 'https:'; // WebCrypto does NOT work without HTTPS!
